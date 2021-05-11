64d35c0149fc044b95d40c572bdcdbd30ddeb851
Fernando <nandosbx@users.noreply.github.com>
2021-05-10 23:45:57 -0300

    Cadastro

---
+++ client/src/components/header.tsx
@@ -0,0 +1,9 @@
+export default function Header() {
+    return (
+        <div className="col-12">
+            <header>
+                <div>Header</div>
+            </header>
+        </div>
+    )
+}
