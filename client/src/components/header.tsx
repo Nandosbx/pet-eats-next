bfb1a8024598045e15da4261a380614f19c26821
Fernando <nandosbx@users.noreply.github.com>
2021-05-12 20:26:03 -0300

    Sass Config

--- client/src/components/header.tsx
+++ client/src/components/header.tsx
@@ -1,9 +1,9 @@
 export default function Header() {
-    return (
-        <div className="col-12">
-            <header>
-                <div>Header</div>
-            </header>
-        </div>
-    )
+  return (
+      <div className="col-12">
+          <header>
+              <div>Header</div>
+          </header>
+      </div>
+  )
 }
